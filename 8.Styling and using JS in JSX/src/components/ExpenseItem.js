df588336033a56a446135c4e3ab33c74f0efba17
MohammadAyubAnsari <mdayubansari2014@gmail.com>
2023-01-30 13:53:48 +0530

    first commit

---
+++ 8.Styling and using JS in JSX/src/components/ExpenseItem.js
@@ -0,0 +1,24 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const LocationOfExpenditure = "On Car";
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  return (
+    <div>
+      <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div>
+          <h2>{LocationOfExpenditure}</h2>
+        </div>
+        <div className="expense-item__description ">
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">${expenseAmount}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
